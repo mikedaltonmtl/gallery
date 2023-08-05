e6119e709dd5c4c43497bdd995f388a24659243f
mike-dalton <michael.j.dalton@gmail.com>
2023-08-05 13:35:46 -0400

    animate view button on splash page

--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Slider({ slides }) {
   return (
     <div id='gallery'>
       <h1>Gallery</h1>
+      {slides.map((slide, index) => {
+        return (
+          <Image
+            key={index}
+            className="image"
+            src={slide}
+            alt={index}
+            placeholder="blur"
+            draggable="false"
+            width='1440'
+            height='600'
+            style={{ objectFit:'cover' }}
+          />
+        );
+      })}
     </div>
   );
 }
--- app/components/Splash.jsx
+++ app/components/Splash.jsx
@@ -7,7 +7,7 @@ export default function Splash({ heading, message }) {
       <div className='p-5 text-white z-10 ml-0 sm:ml-[-10rem] mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>{ heading }</h2>
         <p className='py-5 text-xl'>{ message }</p>
-        <button className='px-8 py-2 border'>View</button>
+        <button className='slidebtn px-8 py-2 border'>View</button>
       </div>
     </main>
   );
