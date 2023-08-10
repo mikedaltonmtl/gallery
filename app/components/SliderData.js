250d8a9245982c75aeb675500666a1da3bc2fafb
mike-dalton <michael.j.dalton@gmail.com>
2023-08-10 13:49:00 -0400

    add image

--- app/components/SliderData.js
+++ app/components/SliderData.js
@@ -28,10 +28,12 @@ import romy027 from '../../public/Romy027.jpg';
 import romy028 from '../../public/Romy028.jpg';
 import romy029 from '../../public/Romy029.jpg';
 import romy030 from '../../public/Romy030.jpg';
+import romy031 from '../../public/Romy031.jpg';
 
 
 export const SliderData = [
   romy001, romy002, romy003, romy004, romy005, romy006, romy007, romy008, romy009, romy010,
   romy011, romy012, romy013, romy014, romy015, romy016, romy017, romy018, romy019, romy020,
-  romy021, romy022, romy023, romy024, romy025, romy026, romy027, romy028, romy029, romy030
+  romy021, romy022, romy023, romy024, romy025, romy026, romy027, romy028, romy029, romy030,
+  romy031
 ];
